54ebd86331fe092dca4a134025df929985fd1253
Jwhyee <82663161+Jwhyee@users.noreply.github.com>
2021-10-27 00:09:33 +0900

    ignore 파일 수정

--- Clang/SelectionSort.c
+++ Clang/SelectionSort.c
@@ -2,16 +2,16 @@
 
 #include <stdio.h>
 int main(){
-    int i, j, t;
+    int i, j, temp;
     int a[5] = {5, 3, 1, 4, 2};
 
 
     for(i = 0; i <= 3; i++){
         for(j = 1; j <= 4; j++){
             if(a[i] > a[j]){
-                t = a[i];
+                temp = a[i];
                 a[i] = a[j];
-                a[j] = t;
+                a[j] = temp;
             }
         }
         printf("%d %d %d %d %d \n", a[0], a[1], a[2], a[3], a[4]);
