2c4768ffeb641fd50d0a758a20467dafa2ae7dd5
Jwhyee <82663161+Jwhyee@users.noreply.github.com>
2021-10-27 00:12:48 +0900

    정렬이 안 되는 버그 수정

--- Clang/SelectionSort.c
+++ Clang/SelectionSort.c
@@ -2,18 +2,17 @@
 
 #include <stdio.h>
 int main(){
-    int i, j, temp;
+    int i, j, t;
     int a[5] = {5, 3, 1, 4, 2};
 
-
     for(i = 0; i <= 3; i++){
-        for(j = 1; j <= 4; j++){
+        for(j = i+1; j <= 4; j++){
             if(a[i] > a[j]){
-                temp = a[i];
+                t = a[i];
                 a[i] = a[j];
-                a[j] = temp;
+                a[j] = t;
             }
         }
-        printf("%d %d %d %d %d \n", a[0], a[1], a[2], a[3], a[4]);
+        printf("%d %d %d %d %d\n", a[0], a[1], a[2], a[3], a[4]);
     }
 }
