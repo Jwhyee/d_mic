dac7431f8a1ed0fc1a08471e3835c69166cd4c12
Jwhyee <82663161+Jwhyee@users.noreply.github.com>
2021-10-26 23:06:27 +0900

    피보나치 변수 재정의

--- Clang/Fibonacci.c
+++ Clang/Fibonacci.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 int main(){
-    int i = 1;
-    int j = 1;
-    int k = 0;
+    int one = 1;
+    // one -> n-2의 값
+
+    int two = 1;
+    // two -> n-1의 값
+
+    int result = 0;
+    // result는 현재값을 의미
+
     int sum = 2;
     for(int cnt = 3; cnt <= 30; cnt++){
-        k = i + j;
-        sum += k;
-        i = j;
-        j = k;
+        result = one + two;
+        sum += result;
+        one = two;
+        two = result;
     }
     printf("30항까지 덧셈 결과 = %d \n", sum);
-    // 주석 추가
 }
