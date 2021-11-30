d0c38b85bd1b5f289264081630951d7e11e021c3
Jwhyee <82663161+Jwhyee@users.noreply.github.com>
2021-12-01 07:28:45 +0900

    알고리즘 강의 내용 추가

---
+++ Clang/while_공백_출력.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i = 0;
+    int max_endl = 10;
+    
+    while(i < max_endl){
+        cout<<"*                              *" << endl;
+        i++;
+    }
+}
